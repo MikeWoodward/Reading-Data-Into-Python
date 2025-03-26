7539a74cf6a7694b78669701af862d2ccf42db65
MikeWoodward <work_woodward@hotmail.com>
2025-03-26 14:18:47 -0400

    Scraping updates

---
+++ Scraping/README.md
@@ -0,0 +1,2 @@
+# Web scraping
+This is the demo code for reading in data scraped from web pages. 
---
+++ Scraping/scraping.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+from csv import writer
+from re import sub
+import requests
+import sys
+
+# Getting web page data
+# =====================
+url = 'https://en.wikipedia.org/wiki/' \
+      'List_of_tallest_buildings_and_structures'
+headers = {'User-Agent': 'Python User Agent example'}
+try:
+    response = requests.get(url, headers=headers, timeout=10)
+except requests.ConnectionError as e:
+    print('Couldn\'t reach the server.')
+    print('Reason: ', e)
+    sys.exit(1)
+except requests.Timeout as e:
+    print('Timeout error.')
+    print('Reason: ', e)
+    sys.exit(1)
+print(response.headers)
+print(response.status_code)
+html = response.text
+
+# Parse web data
+# ==============
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find_all('table')[2]
+rows = table.find_all('tr')
+
+buildings = []
+
+# Headings
+buildings.append([header.string for header in rows[0].find_all('th')])
+
+# Data
+for row in rows[1:]:
+    items = row.find_all('td')
+    line1 = [sub(r'\[\d+\]', r'', item.text.strip())
+             for item in items[:-1]]
+    loc = items[-1].find('span', {'class': 'geo'})
+    loc_text = '' if loc is None else loc.text.strip()
+
+    buildings.append(line1 + [loc_text])
+
+# Output data
+# ===========
+with open("buildings.csv", 'w') as file_obj:
+    csv_file = writer(file_obj)
+    for row in buildings:
+        row = [s.encode('utf-8') for s in row]
+        csv_file.writerow(row)
