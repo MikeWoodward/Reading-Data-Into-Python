e4dfae467fff2ffb3c1543d5c89200fca333a0a6
MikeWoodward <work_woodward@hotmail.com>
2026-03-02 14:56:21 -0500

    Updates

--- CSV/README.md
+++ CSV/README.md
@@ -1,2 +1,8 @@
 # CSV data
-This is the demo code for the chpater on reading in CSV data. 
+This is the demo code for the chapter on reading in CSV data. 
+
+To get the data for this example:
+*  go to https://bmrs.elexon.co.uk/rolling-system-demand
+* you should see a chart of the last 24 hours of UK electricity demand
+* on the bottom right of the page, you should see an “Export” button, 
+  click on that and choose the CSV download.
--- CSV/csv.py
+++ CSV/csv.py
@@ -1,6 +1,14 @@
 import datetime
 import csv
 
+"""
+To get the data for this example:
+  go to https://bmrs.elexon.co.uk/rolling-system-demand
+  you should see a chart of the last 24 hours of UK electricity demand
+  on the bottom right of the page, you should see an “Export” button, 
+  click on that and choose the CSV download.
+"""
+
 FILENAME = ("""RollingSystemDemand-2025-11-24"""
             """T21_30_00.000Z-2025-11-25"""
             """T21_30_00.000Z.csv""")
